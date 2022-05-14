e9228a78f94e5c76c9623f07117c725633a54cd3
Echommx <105528610+Echommx@users.noreply.github.com>
2022-05-14 20:57:23 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"echo_mingxia9630@outlook.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
